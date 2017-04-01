6bce8d1e4d60f76c9d1d86d0797d42344abe34e3
shaybanerjee <shayon@live.ca>
2017-03-31 21:59:23 -0400

    Updated Grid

--- grid.cc
+++ grid.cc
@@ -1,38 +1,62 @@
-/*
-class Grid {
-	std::vector <std::vector <GameSubject*>> theGrid; // vector of vector of GameSubjects
-	Chamber cham_arr[5]; 
-//	std::vector <Position> allPositions; // all positions in chamber (for moving enemies etc) optimization
-	// what we could do instead is loop through theGrid check if something is enemy and move accordingly. 
-	int gridSize; // size of grid
-	GameSubject *PC; 
-public: 
-	Grid(int n, ifstream &f); // grid constructor 
-	~Grid(); // grid destructor
-	void rand_fill(); // fill up chambers randomly
-	void check_radius(); // should be called before and after move()
-	void move_enemies(); // move all enemies. 
-	void move(Direction d); 
-	std::ostream &operator<<(std::ostream& out, const theGrid); // output the grid
-};
-*/
-
 #include "grid.h"
+#include "hwall.h"
+#include "vwall.h"
+#include "floor.h"
+#include "passageway.h"
+#include "door.h"
+#include "empty.h"
 #include <iostream>
 
 using namespace std; 
 
 
-Grid::Grid(int n, ifstream &f) 
-	: gridSize{n}, f{f} {
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-
+Grid::Grid(ifstream &f, GameSubject& PC) 
+	:width{79}, height{25}, f{f}, PC{PC} {
+	char curr_char; 
+	for (int row = 0; row < height; ++row) {
+		for (int col = 0; col < width; ++col) {
+			f >> curr_char; 
+			if (curr_char == '-') {
+				GameSubject* gameSub = new Hwall(i,j);
+				theGrid[row][col] = gameSub;   
+			}
+			else if (curr_char == '|') {
+				GameSubject* gameSub = new Vwall(i, j); 
+				theGrid[row][col] = gameSub; 
+			}
+			else if (curr_char == '.') {
+				GameSubject* gameSub = new Floor(i, j);
+				theGrid[row][col] = gameSub;  
+			}
+			else if (curr_char == '#') {
+				GameSubject* gameSub = new PassageWay(i, j);
+				theGrid[row][col] = gameSub;  
+			}
+			else if (curr_char == '+') {
+				GameSubject* gameSub = new Door(i, j); 
+				theGrid[row][col] = gameSub; 
+			}
+			else if (curr_char == ' ') {
+				GameSubject* gameSub = new Empty(i, j);
+				theGrid[row][col] = gameSub;  
+			}
 		}
-
 	}
-
+	for (int i = 0; i < 5; ++i) {
+		cham_arr[i] = Chamber{i};
+	}
 }
+
+void Grid::clearGrid() {
+	for (int row = 0; i < height; ++row) {
+		for (int col = 0; col < width; ++col) {
+			GameSubject* currSub = theGrid[i][j]; 
+			
+		}
+	}
+}
+
+
 
 ostream &operator<<(ostream &out, const theGrid) {
 	for (int i = 0; i < n; ++i) {
--- grid.h
+++ grid.h
@@ -4,6 +4,7 @@
 #include "gamesubject.h"
 #include "direction.h"
 #include "position.h"
+#include "playerCharacter.h"
 #include "chamber.h"
 
 class GameSubject; 
@@ -25,17 +26,23 @@ class Grid {
 	std::vector <std::vector <GameSubject*>> theGrid; // vector of vector of GameSubjects
 	Chamber cham_arr[5]; 
 	ifstream &f; 
-//	std::vector <Position> allPositions; // all positions in chamber (for moving enemies etc) optimization
 	// what we could do instead is loop through theGrid check if something is enemy and move accordingly. 
-	int gridSize; // size of grid
-	GameSubject *PC; 
+	const int width; // size of width
+	const int hight; // size of heigth 
+	GameSubject &PC; 
+	int level; 
 public: 
-	Grid(int n, ifstream &f); // grid constructor 
+	Grid(ifstream &f, GameSubject& PC); // grid constructor 
 	~Grid(); // grid destructor
-	void rand_fill(); // fill up chambers randomly
-	void check_radius(); // should be called before and after move()
+	void rand_enemy(); // fill up chambers with enemies randomly
+	void rand_potion(); // fill up chambers with potions randomly
+	void rand_tresure(); // fill up chambers with tresures randomly
 	void move_enemies(); // move all enemies. 
-	void move(Direction d); 
+	void usePotion(Direction d); // use potion in that direction
+	void atkEnemy(Direction d); // attack enemy in that direction
+	void atkByEnemy(); // checks radius if enemy should attack
+	void move(Direction d); // player Player 
+	void clear_grid(); // clear the entire grid for a new level 
 	std::ostream &operator<<(std::ostream& out, const theGrid); // output the grid
 };
 
