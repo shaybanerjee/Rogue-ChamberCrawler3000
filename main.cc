fd7c5a4fc06e53193ea846e740ff0b8620a9fed9
weiliam <william.zhu97@gmail.com>
2017-04-01 00:44:40 -0400

    added choose character in main.cc

--- main.cc
+++ main.cc
@@ -1,7 +1,60 @@
+#include <string>
+#include <iostream>
+#include "grid.h"
+#include "drow.h"
+#include "shade.h"
+#include "goblin.h"
+#include "vampire.h"
+#include "troll.h"
+char playerSelect(){ // Player selects race to play as 
+	char character;
+	cout << "Please select a race for you player: "<<endl;
+	cout << "Drow: 'd'"<<endl;
+	cout << "Goblin: 'g'"<<endl;
+	cout << "Shade: 's'"<<endl;
+	cout << "Troll: 't'"<<endl;
+	cout << "Vampire: 'v'"<<endl;
 
+	cin >> character; // reads in selected race
+
+	while(character != 'd' && character != 'g' && character != 's'
+		&& character != 't' && character != 'v'){
+		cout << "Invalid Selection. Please try again with one of";
+		cout << " the above selections (d, g, s, t, v)"<<endl;
+		cin >> character;
+	}
+	return character;
+
+
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+	char race = playerSelect(); // selects players race
+	string file = "cc3kfloor.txt";
+	if (argc == 1){
+		file = argv[1];
+	}
+
+	Grid g{file};
+	PlayerCharacter *pc;
+
+	switch(race){
+		case 'd':
+			pc = new Drow();
+			break;
+		case 'g':
+			pc = new Goblin();
+			break;
+		case 's':
+			pc = new Shade();
+			break;
+		case 't':
+			pc = new Troll();
+			break;
+		case 'v':
+			pc = new Vampire();
+			break;
+	}
+
 }
